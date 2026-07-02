fe2bee9d719a784364cf7d15dda156f4d10599e1
Ngan22130178 <146084366+Ngan22130178@users.noreply.github.com>
2026-07-02 20:51:58 +0700

    Thêm controller của ad

--- src/main/java/vn/edu/nlu/fit/musicweb/controller/admin/AdminController.java
+++ src/main/java/vn/edu/nlu/fit/musicweb/controller/admin/AdminController.java
@@ -1,15 +1,26 @@
 package vn.edu.nlu.fit.musicweb.controller.admin;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import vn.edu.nlu.fit.musicweb.repository.SongRepository;
 import vn.edu.nlu.fit.musicweb.service.MusicScannerService;
-import java.io.IOException;
 
-@RestController
+@Controller
+@RequestMapping("/admin") // Ánh xạ tới /admin
 public class AdminController {
 
-}
-    
+    private final SongRepository songRepository;
 
+    public AdminController(SongRepository songRepository) {
+        this.songRepository = songRepository;
+    }
+
+    @GetMapping
+    public String adminDashboard(Model model) {
+        // Lấy danh sách từ database
+        model.addAttribute("songs", songRepository.findAll());
+        // Trả về file JSP (đảm bảo file nằm ở /WEB-INF/views/admin.jsp)
+        return "admin/admin"; 
+    }
+}
