8a09f0b93067135f663c479d5cacabfdafd3674d
Ngan22130178 <146084366+Ngan22130178@users.noreply.github.com>
2026-07-03 00:35:40 +0700

    Reformat AdminController with consistent spacing

--- src/main/java/vn/edu/nlu/fit/musicweb/controller/admin/AdminController.java
+++ src/main/java/vn/edu/nlu/fit/musicweb/controller/admin/AdminController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import vn.edu.nlu.fit.musicweb.repository.SongRepository;
-import vn.edu.nlu.fit.musicweb.service.MusicScannerService;
+
 
 @Controller
 @RequestMapping("/admin") // Ánh xạ tới /admin
